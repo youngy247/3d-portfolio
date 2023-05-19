70002bbfc9de8611e4564a68b3845a7bd9a0380f
Adam Young <adamyoungy678@gmail.com>
2023-05-19 08:12:47 +0100

    feat: added hamburger menu for phone screen sizes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ import {logo, menu, close} from '../assets';
 
 const Navbar = () => {
   const [active, setActive] = useState('')
+  const [toggle, setToggle] = useState(true);
   return (
       <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
         <div className="w-full flex 
@@ -39,6 +40,15 @@ const Navbar = () => {
                 </li>
               ))}
             </ul>
+            <div className="sm:hidden flex flex-1 justify-end items-center">
+            <img 
+            src={menu} 
+            alt="menu"
+            className="w-[28px] h-[28px]
+            object-contain cursor pointer"
+              onClick={() => setToggle(!toggle)}
+            />
+            </div>
         </div>
       </nav>
   )
