f3af8a6a1a1ed4d1da85375f70964d9fb6611454
Adam Young <adamyoungy678@gmail.com>
2023-05-20 16:02:16 +0100

    (refactor)refactoring files after 3d model rendering issue

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,7 +4,8 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
 const Computers = () => {
-  const computer = useGLTF('./desktop_pc/scene.gltf')
+  const computer = useGLTF('./desktop_pc/scene.gltf',)
+  console.log(computer)
   return (
     <mesh>
       <hemisphereLight intensity={0.15}
