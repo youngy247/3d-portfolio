eab748521584c4038115605f1f45322dc5ad7d1e
Adam Young <adamyoungy678@gmail.com>
2023-05-20 12:40:38 +0100

    feat: added name and web dev in a flex to be in a single line and fixed mistake in the parent container max width

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
         <div className="w-full flex 
                       justify-between 
                       items-center
-                       max-w-7x1 
+                       max-w-7xl 
                        mx-auto">
             <Link to="/" className="flex items-center gap-2"
             onClick={() => {
@@ -22,7 +22,10 @@ const Navbar = () => {
             }}
             >
               <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-            <p className="text-white text-[18px] font-bold cursor-pointer">Adam <span className="sm:block hidden">| Web Developer</span></p>
+            <p className="text-white text-[18px] font-bold cursor-pointer flex">
+            Adam &nbsp;
+            <span className="sm:block hidden">| Web Developer</span>
+            </p> 
             </Link>
             <ul className="list-none hidden sm:flex flex-row gap-10">
               {navLinks.map((link)=>(
