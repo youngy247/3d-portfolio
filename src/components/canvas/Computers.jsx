3e53480deb75cc2a19383381ca122e56bf8bf3e7
Adam Young <adamyoungy678@gmail.com>
2023-05-20 14:34:35 +0100

    feat: Adjusted rotation of 3D model in computers component

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -14,6 +14,7 @@ const Computers = () => {
         object={computer.scene}
           scale={0.75}
           position={[0, -3.25, -1.5]}
+          rotation={[-0.01, -0.2, -0.1]}
         />
     </mesh>
   )
