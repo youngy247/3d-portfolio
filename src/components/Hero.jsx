20333835632c1369d1403bc8f1de664d55e47a48
Adam Young <adamyoungy678@gmail.com>
2023-05-20 16:09:57 +0100

    feat: added animation for scroll wheel

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,7 +24,19 @@ const Hero = () => {
       items-center">
         <a href="#about"> 
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary
-          flex justify-center items-start p-2"></div>
+          flex justify-center items-start p-2">
+            <motion.dev 
+              animate={{
+                y: [0, 24, 0]
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop'
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
         </a>
       </div>
     </section>
