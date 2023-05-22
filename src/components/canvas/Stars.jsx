7281ce1bd4b53894888eb466c062c2a990e782c2
Adam Young <adamyoungy678@gmail.com>
2023-05-22 20:57:19 +0100

    feat applied rotation to group component and adjusted styling of star canvas

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -9,7 +9,7 @@ const Stars = (props) => {
   const sphere = random.inSphere(new Float32Array (5000), { radius: 1.2 })
 
   return (
-    <group>
+    <group rotation={[0, 0, Math.PI / 4]}>
       <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
         <PointMaterial
           transparent
@@ -25,7 +25,7 @@ const Stars = (props) => {
 
 const StarsCanvas = () => {
   return (
-    <div className="w-full h-full absolute inset-0 z-[-1]">
+    <div className="w-full h-auto absolute inset-0 z-[-1]">
       <Canvas camera={{ position: [0, 0, 1] }}>
         <Suspense fallback={null}>
           <Stars />
