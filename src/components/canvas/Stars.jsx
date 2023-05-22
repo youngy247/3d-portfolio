009556651d720f6609f8398c941038c09c45ec21
Adam Young <adamyoungy678@gmail.com>
2023-05-22 20:23:19 +0100

    feat: added star background to canvas

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -12,7 +12,13 @@ const Stars = (props) => {
 const StarsCanvas = () => {
   return (
     <div className="w-full h-full absolute inset-0 z-[-1]">
-      
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <Suspense fallback={null}>
+          <Stars />
+        </Suspense>
+        
+        <Preload all />
+      </Canvas>
     </div>
   )
 }
