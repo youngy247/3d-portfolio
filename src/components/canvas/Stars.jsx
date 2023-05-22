067c631121e1c8aeb0c64e93b6cd615112e6ac8a
Adam Young <adamyoungy678@gmail.com>
2023-05-22 20:52:14 +0100

    feat: render random stars using Points component

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -4,8 +4,14 @@ import { Points, PointMaterial, Preload } from '@react-three/drei'
 import * as random from 'maath/random/dist/maath-random.esm'
 
 const Stars = (props) => {
+  const ref = useRef()
+
+  const sphere = random.inSphere(new Float32Array (5000), { radius: 1.2 })
+
   return (
-    <div>Stars</div>
+    <group>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}/>
+    </group>
   )
 }
 
@@ -16,7 +22,7 @@ const StarsCanvas = () => {
         <Suspense fallback={null}>
           <Stars />
         </Suspense>
-        
+
         <Preload all />
       </Canvas>
     </div>
