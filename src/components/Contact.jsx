d50285f9087ad94f22cd0471ebfe1da97f7640b8
Adam Young <adamyoungy678@gmail.com>
2023-05-22 18:36:50 +0100

    feat: added responsivesness for earth model and imported the earth canvas into contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -99,8 +99,9 @@ const handleSubmit = (e) => {
       </motion.div>
       <motion.div 
       variants={slideIn('right', "tween", 0.2, 1)}
+      className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
       >
-
+        <EarthCanvas />
       </motion.div>
     </div>
   )
