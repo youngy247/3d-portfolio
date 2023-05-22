aa3bbe13094798b36e6f39ed562233b17f48e99a
Adam Young <adamyoungy678@gmail.com>
2023-05-22 13:14:10 +0100

    progress: added empty tilt animation cards for projects

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -11,7 +11,19 @@ import { fadeIn, textVariant } from '../utils/motion'
 const ProjectCard = ( { index, name, description, tags, image, source_code_link }) => {
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-      test
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450
+        }}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+      >
+        <div className="relative w-full h-[230px]">
+
+        </div>
+
+      </Tilt>
     </motion.div>
   )
 }
