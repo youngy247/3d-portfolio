0c170e41948b0232a33126e081d0a56544e2d22d
Adam Young <adamyoungy678@gmail.com>
2023-05-22 12:52:37 +0100

    feat: added project section titles

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -10,7 +10,12 @@ import { fadeIn, textVariant } from '../utils/motion'
 const Works = () => {
   return (
     <>
-
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>
+        My work</p>
+        <h2 className={styles.sectionHeadText}
+        >Projects.</h2>
+      </motion.div>
     </>
   )
 }
