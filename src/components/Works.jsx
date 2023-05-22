a6ed40ec10c83d1bb66ef2b06cc125507597327b
Adam Young <adamyoungy678@gmail.com>
2023-05-22 13:05:59 +0100

    progress: created containers for projects to go in

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -7,6 +7,7 @@ import { SectionWrapper } from '../hoc'
 import { projects } from '../constants'
 import { fadeIn, textVariant } from '../utils/motion'
 
+
 const Works = () => {
   return (
     <>
@@ -23,6 +24,17 @@ const Works = () => {
         className="mt-3 text-secondary text-[17px] max-w-3x1 leading-[30px]">
         My portfolio features a collection of projects that demonstrate my skills and expertise with real-world examples. Each project is accompanied by a brief description and includes links to the code repositories and live demos. These projects serve as evidence of my capability to tackle complex problems, proficiency with various technologies, and effective project management.
         </motion.p>
+      </div>
+
+      <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((project, index) => (
+          <ProjectCard
+            key={`project-${index}`}
+            index={index}
+            {...project}
+          />
+        ))}
+      
       </div>
     </>
   )
