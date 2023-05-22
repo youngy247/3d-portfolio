fb49278ccddadb612f3a3b33879012be7b6aeef3
Adam Young <adamyoungy678@gmail.com>
2023-05-22 18:00:52 +0100

    feat: added animation to the contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -25,7 +25,13 @@ const handleSubmit = (e) => {
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+      <motion.div
+      variants={slideIn('left', "tween", 0.2, 1)}
+      className="flex-[0.75] bg-black p-8 rounded-2xl"
+      >
 
+
+      </motion.div>
     </div>
   )
 }
