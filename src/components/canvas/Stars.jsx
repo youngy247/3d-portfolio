3b1008ceeb514a21b3739ed4660d74dec1ba19b8
Adam Young <adamyoungy678@gmail.com>
2023-05-22 20:21:01 +0100

    feat: created stars canvas and set it to display behind the earth model

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -3,10 +3,18 @@ import { Canvas, useFrame } from '@react-three/fiber'
 import { Points, PointMaterial, Preload } from '@react-three/drei'
 import * as random from 'maath/random/dist/maath-random.esm'
 
-const Stars = () => {
+const Stars = (props) => {
   return (
     <div>Stars</div>
   )
 }
 
-export default Stars
+const StarsCanvas = () => {
+  return (
+    <div className="w-full h-full absolute inset-0 z-[-1]">
+      
+    </div>
+  )
+}
+
+export default StarsCanvas
