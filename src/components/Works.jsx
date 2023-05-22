8de7f509c8620aa4c48e96754a7bfd7a7d6338cc
Adam Young <adamyoungy678@gmail.com>
2023-05-22 17:30:03 +0100

    progress: added comment to add live link to projects later

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -36,6 +36,16 @@ const ProjectCard = ( { index, name, description, tags, image, source_code_link 
                 className="w-1/2 h-1/2 object-contain"
               />
             </div>
+            {/* <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img 
+                src={github}
+                alt="github link"
+                className="w-1/2 h-1/2 object-contain"
+              />
+            </div> */}
 
           </div>
 
