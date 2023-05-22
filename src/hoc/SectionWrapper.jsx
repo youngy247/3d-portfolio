fcbaa6e902c58e75a1aec0b43bf80603de98c181
Adam Young <adamyoungy678@gmail.com>
2023-05-22 12:48:22 +0100

    refactor: adjusted spacing and correcting spelling mistake in sectionWrapper

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -1,28 +1,25 @@
-import React from 'react';
-import { motion } from 'framer-motion';
-import { styles } from '../styles';
-import { staggerContainer } from '../utils/motion';
+import { motion } from 'framer-motion'
+import { styles } from '../styles'
+import { staggerContainer } from '../utils/motion'
 
 const SectionWrapper = (Component, idName) => {
-  function HOC() {
-    return (
-      <motion.section
+    function HOC() {
+        return (
+        <motion.section
         variants={staggerContainer()}
         initial="hidden"
         whileInView="show"
         viewport={{ once: true, amount: 0.25 }}
         className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
-      >
+        >
         <span className="hash-span" id={idName}>
-          &nbsp;
+            &nbsp;
         </span>
-        <motion.div variants={staggerContainer()} className="cards-wrapper">
-          <Component />
-        </motion.div>
-      </motion.section>
-    );
-  }
-  return HOC;
-};
+        <Component />
+        </motion.section>
+    )
+    }
+    return HOC
+}
 
-export default SectionWrapper;
+export default SectionWrapper
