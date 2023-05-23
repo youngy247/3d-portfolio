f7578992bdc779d0af6f43dcaa24d357de9edf5c
Adam Young <adamyoungy678@gmail.com>
2023-05-23 16:27:12 +0100

    feat: added each paragraph of overview into seperate motion divs and increased time for each to load for effect

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,11 +47,23 @@ const About = () => {
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
       I am a highly motivated and passionate full stack developer, skilled in TypeScript, JavaScript, PHP, MySQL, and the MERN stack. With a strong foundation in these technologies, I am equipped to build robust and scalable applications.
-
+      </motion.p>
+      <motion.p
+        variants={fadeIn("", "", 0.2, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
       Driven by my love for learning and innovation, I continuously stay updated with the latest industry trends and best practices. I thrive in dynamic and collaborative environments, where I can utilize my problem-solving abilities to create impactful solutions.
-
+</motion.p>
+<motion.p
+        variants={fadeIn("", "", 0.3, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
       My goal is to deliver user-friendly and efficient applications that solve real-world problems. With a keen eye for detail and a dedication to crafting elegant designs, I strive to create seamless user experiences.
-
+</motion.p>
+<motion.p
+        variants={fadeIn("", "", 0.4, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
       I am excited about the opportunity to contribute to forward-thinking organizations and make a meaningful impact. Let's work together to bring your ideas to life and achieve success in the digital realm.
       </motion.p>
 
