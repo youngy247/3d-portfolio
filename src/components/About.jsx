5ea204487b62c5a09e76f75c7c2d4c8b22cbe245
Adam Young <adamyoungy678@gmail.com>
2023-05-27 22:21:54 +0100

    fix: reduced size of service card and project containers to all fit in one row on macbook screens

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ import { SectionWrapper } from '../hoc'
 
 const ServiceCard = ({index, title, icon}) => {
   return (
-    <Tilt className="xs:w-[250px] w-full">
+    <Tilt className="xs:w-[225px] w-full">
     <motion.div
       variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
       className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
