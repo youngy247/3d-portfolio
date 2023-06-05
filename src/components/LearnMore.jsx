85ddfab6447c914edc37758465c2a46d2a8f38e6
Adam Young <adamyoungy678@gmail.com>
2023-06-05 11:17:40 +0100

    chore: added imports and wraped LearnMore component in SectionWrapper HOC

---
+++ src/components/LearnMore.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+import { textVariant } from '../utils/motion'
+import { styles } from '../styles'
+import { SectionWrapper } from '../hoc'
+
+const LearnMore = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default SectionWrapper(LearnMore, "learn")
