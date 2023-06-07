b6b05faf06660c7a5ba6b8f74847ed6af29ef527
Adam Young <adamyoungy678@gmail.com>
2023-06-07 17:25:31 +0100

    feat: added animation states of modal

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -29,6 +29,9 @@ const Modal = ({ handleClose, text }) => {
                 onClick={(e) => e.stopPropagation()}
                 className="modal orange-gradient"
                 variants={dropIn}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
             >
 
             </motion.div>
