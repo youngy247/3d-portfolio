3819ededf9e2921d5fce37cecc90630c63c50f48
Adam Young <adamyoungy678@gmail.com>
2023-06-07 17:02:32 +0100

    chore: created backdrop component and added imports

---
+++ src/components/backdrop.jsx
@@ -0,0 +1,12 @@
+import { motion } from "framer-motion"
+
+const Backdrop = ({ children, onClick }) => {
+
+    return (
+        <motion.div>
+            
+        </motion.div>
+    )
+}
+
+export default Backdrop
