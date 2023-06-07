1b9a487ff37ab684db3c6ccd72616ca6c3ef7bd5
Adam Young <adamyoungy678@gmail.com>
2023-06-07 17:11:57 +0100

    chore: added modal component with imports

---
+++ src/components/modal.jsx
@@ -0,0 +1,12 @@
+import { motion } from 'framer-motion'
+import Backdrop from './backdrop'
+
+const Modal = ({ handleClose, text }) => {
+    return(
+        <>
+
+        </>
+    )
+}
+
+export default Modal
