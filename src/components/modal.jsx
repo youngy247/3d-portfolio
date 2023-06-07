849b0184953e904c765a6448717e073dc0e85645
Adam Young <adamyoungy678@gmail.com>
2023-06-07 17:24:33 +0100

    feat: created dropIn animation for modal

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,12 +1,34 @@
 import { motion } from 'framer-motion'
 import Backdrop from './backdrop'
 
+const dropIn = {
+    hidden: {
+        y: "-100vh",
+        opacity: 0,
+    },
+    visible: {
+        y: "0",
+        opacity: 1,
+        transition: {
+            duration: 0.1,
+            type: "spring",
+            damping: 0,
+            stiffness: 500,
+        }
+    },
+    exit: {
+        y: "100vh",
+        opacity: 0,
+    },
+}
+
 const Modal = ({ handleClose, text }) => {
     return(
         <Backdrop onClick={handleClose}>
             <motion.div
                 onClick={(e) => e.stopPropagation()}
                 className="modal orange-gradient"
+                variants={dropIn}
             >
 
             </motion.div>
