fd518bec821043ff3943a2f7b3d1b529faf44e89
Adam Young <adamyoungy678@gmail.com>
2023-06-07 17:15:48 +0100

    feat: Implemented backdrop into modal with clickable functionality

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -3,9 +3,14 @@ import Backdrop from './backdrop'
 
 const Modal = ({ handleClose, text }) => {
     return(
-        <>
+        <Backdrop onClick={handleClose}>
+            <motion.div
+                onClick={(e) => e.stopPropagation()}
+                className="modal orange-gradient"
+            >
 
-        </>
+            </motion.div>
+        </Backdrop>
     )
 }
 
