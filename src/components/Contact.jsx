c7f36210e5dbc3d6d06f8d9a53e7ee72e30c32da
Adam Young <adamyoungy678@gmail.com>
2023-06-11 18:45:33 +0100

    chore: removed unused emailjs import

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect, useRef } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
-import emailjs from '@emailjs/browser'
 import { styles } from '../styles'
 import { EarthCanvas } from './canvas'
 import { SectionWrapper } from '../hoc'
