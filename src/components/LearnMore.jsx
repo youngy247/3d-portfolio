c08e486bc23ce95369f008b5cf35c30f1395fdcf
Adam Young <adamyoungy678@gmail.com>
2023-06-17 19:43:21 +0100

    feat: conditionally rendered nexus iFrame to be wider on mobile

--- src/components/LearnMore.jsx
+++ src/components/LearnMore.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { motion } from 'framer-motion'
 import { textVariant } from '../utils/motion'
 import { styles } from '../styles'
 import { SectionWrapper } from '../hoc'
 
 const LearnMore = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
+
+    setIsMobile(mediaQuery.matches);
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener('change',handleMediaQueryChange);
+    return () => {
+      mediaQuery.removeEventListener('change',handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <>
       <motion.div variants={textVariant()}>
@@ -15,7 +31,7 @@ const LearnMore = () => {
       </motion.div>
       <div className="flex items-center justify-center h-screen">
         <iframe
-          className="w-5/6 h-3/4"
+          className={isMobile ? 'w-full h-3/4' : 'w-5/6 h-3/4'}
           src="https://nexusbot-beta.netlify.app/"
           title="Adam's personal assistant chatbot Nexus"
         ></iframe>
