45d7a65ca4a3ca772cc01d309480fcf7efd10661
Adam Young <adamyoungy678@gmail.com>
2023-07-17 19:24:33 +0100

    refactor: changed the wheel animation to use a link element instead of anchor tag to remove hash from url

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,18 @@
-import {motion} from 'framer-motion';
-import {styles} from '../styles';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
 import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
+
+  const handleAnchorClick = (event) => {
+    event.preventDefault();
+    const targetSection = document.getElementById('about');
+    if (targetSection) {
+      targetSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <section className="relative w-full h-screen mx-auto">
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
@@ -22,7 +32,7 @@ const Hero = () => {
       <div className="absolute xs:bottom-5 
       bottom-32 w-full flex justify-center 
       items-center">
-        <a href="#about"> 
+        <Link to="/about" onClick={handleAnchorClick}> 
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary
           flex justify-center items-start p-2">
             <motion.dev 
@@ -37,7 +47,7 @@ const Hero = () => {
               className="w-3 h-3 rounded-full bg-secondary mb-1"
             />
           </div>
-        </a>
+        </Link>
       </div>
     </section>
   )
