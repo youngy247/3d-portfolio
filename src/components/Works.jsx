76bf95e7d7fc3f84d1fec62f9c207a58929a1912
Adam Young <adamyoungy678@gmail.com>
2023-09-04 10:08:34 +0100

    feat: condtionally show live link/github button if they have links available

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,114 +1,113 @@
-import  Tilt  from 'react-parallax-tilt'
-import { motion } from 'framer-motion'
+import Tilt from "react-parallax-tilt";
+import { motion } from "framer-motion";
 
-import { styles } from '../styles'
-import { github } from '../assets'
-import { live } from '../assets'
-import { SectionWrapper } from '../hoc'
-import { projects } from '../constants'
-import { fadeIn, textVariant } from '../utils/motion'
+import { styles } from "../styles";
+import { github } from "../assets";
+import { live } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
-
-const ProjectCard = ( { index, name, description, tags, image, source_code_link, live_code_link }) => {
+const ProjectCard = ({
+  index,
+  name,
+  description,
+  tags,
+  image,
+  source_code_link,
+  live_code_link,
+}) => {
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
       <Tilt
         options={{
           max: 45,
           scale: 1,
-          speed: 450
+          speed: 450,
         }}
         className="bg-tertiary p-5 rounded-2xl sm:w-[320px] w-full"
       >
         <div className="relative w-full h-[230px]">
-            <img 
-              src={image}
-              alt={name}
-              className="w-full h-full object-cover rounded-2xl"
-            />
+          <img
+            src={image}
+            alt={name}
+            className="w-full h-full object-cover rounded-2xl"
+          />
           <div className="absolute inset-0 flex justify-between m-3 card-img_hover">
-            <div
-              onClick={() => window.open(live_code_link, "_blank")}
-              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-            >
-              <img 
-                src={live}
-                alt="Live link"
-                className="w-1/2 h-1/2 object-contain"
-              />
-            </div>
-            
-            <div
-              onClick={() => window.open(source_code_link, "_blank")}
-              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-            >
-              <img 
-                src={github}
-                alt="github link"
-                className="w-1/2 h-1/2 object-contain"
-              />
-            </div>
-          </div>
-         </div>
-         
-         <div className="mt-5"> 
-            <h3 className="text-white font-bold text-[24px]">
-            {name}
-            </h3>
-            <p className="mt-2 text-secondary text-[14px]">
-            {description}
-            </p>
-         </div>
-
-
-         <div className="mt-4 flex flex-wrap gap-2">
-            {tags.map((tag) => (
-              <p key={tag.name}
-                 className={`text-[14px] ${tag.color}`}
+            {live_code_link && (
+              <div
+                onClick={() => window.open(live_code_link, "_blank")}
+                className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
               >
+                <img
+                  src={live}
+                  alt="Live link"
+                  className="w-1/2 h-1/2 object-contain"
+                />
+              </div>
+            )}
+
+            {source_code_link && (
+              <div
+                onClick={() => window.open(source_code_link, "_blank")}
+                className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+              >
+                <img
+                  src={github}
+                  alt="github link"
+                  className="w-1/2 h-1/2 object-contain"
+                />
+              </div>
+            )}
+          </div>
+        </div>
+
+        <div className="mt-5">
+          <h3 className="text-white font-bold text-[24px]">{name}</h3>
+          <p className="mt-2 text-secondary text-[14px]">{description}</p>
+        </div>
+
+        <div className="mt-4 flex flex-wrap gap-2">
+          {tags.map((tag) => (
+            <p key={tag.name} className={`text-[14px] ${tag.color}`}>
               #{tag.name}
-
-              </p>
-            ))}
-
-         </div>
-
-
+            </p>
+          ))}
+        </div>
       </Tilt>
     </motion.div>
-  )
-}
+  );
+};
 
 const Works = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>
-        My work</p>
-        <h2 className={styles.sectionHeadText}
-        >Projects.</h2>
+        <p className={styles.sectionSubText}>My work</p>
+        <h2 className={styles.sectionHeadText}>Projects.</h2>
       </motion.div>
 
       <div className="w-full flex">
-        <motion.p 
-        variants={fadeIn("", "", 0.1, 1)}
-        className="mt-3 text-secondary text-[17px] max-w-3x1 leading-[30px]">
-        My portfolio features a collection of projects that demonstrate my skills and expertise with real-world examples. Each project is accompanied by a brief description and includes links to the code repositories and live demos. These projects serve as evidence of my capability to tackle complex problems, proficiency with various technologies and effective project management.
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3x1 leading-[30px]"
+        >
+          My portfolio features a collection of projects that demonstrate my
+          skills and expertise with real-world examples. Each project is
+          accompanied by a brief description and includes links to the code
+          repositories and live demos. These projects serve as evidence of my
+          capability to tackle complex problems, proficiency with various
+          technologies and effective project management.
         </motion.p>
       </div>
 
       <div className="mt-20 flex flex-wrap gap-7">
         {projects.map((project, index) => (
-          <ProjectCard
-            key={`project-${index}`}
-            index={index}
-            {...project}
-          />
+          <ProjectCard key={`project-${index}`} index={index} {...project} />
         ))}
-      
       </div>
     </>
-  )
-}
+  );
+};
 
-export default SectionWrapper(Works, "projects")
+export default SectionWrapper(Works, "projects");
